246fa7ba3c9f922ee899671dc450de23b2235b8a
dinos-cyber <phcv623@gmail.com>
2024-03-31 17:50:39 +0700

    Docker network configurations

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,7 @@ import productService from "../services/productService";
 
 const getIndex = async (req, res) => {
   const products = await productService.getAllProducts();
-  console.log(products);
+  // console.log(products);
   res.render("pages/index/index", {
     products: products,
   });
